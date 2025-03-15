6cd2d96cab6007a9dc9a939b001e7892968d6e21
Yash Bhawsar <yashbhawsar97@gmail.com>
2025-03-15 18:00:22 +0530

    done

--- fm/src/App.jsx
+++ fm/src/App.jsx
@@ -1,35 +1,97 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { motion } from "motion/react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  motion;
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <h1 className="text-white text-6xl font-bold ">Yash Bhawsar</h1>
+      <p className="text-gray-600 text-2xl pt-8">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloremque,
+        culpa. Dolores voluptatibus eaque eligendi maiores nisi alias quo,
+        exercitationem necessitatibus unde omnis, doloremque sint laudantium
+        doloribus officiis eum, distinctio quos dolorum facilis consequuntur
+        quidem nulla ullam voluptas? Illo deserunt nulla corrupti soluta
+        perferendis rerum rem, facere eum eos aperiam unde accusantium!
+        Consequuntur non dolorum inventore odit labore nihil doloremque
+        accusantium quaerat placeat voluptatibus, minus illo cumque nemo ratione
+        fugiat? Vero, aut. Illo incidunt quas ipsam facilis provident laboriosam
+        sed eaque enim repudiandae ratione. Nulla rem accusantium, est sapiente
+        eum ducimus quod ea assumenda repellendus itaque optio officiis sed,
+        doloremque iusto?
+      </p>
+      <p className="text-gray-600 text-2xl pt-8">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloremque,
+        culpa. Dolores voluptatibus eaque eligendi maiores nisi alias quo,
+        exercitationem necessitatibus unde omnis, doloremque sint laudantium
+        doloribus officiis eum, distinctio quos dolorum facilis consequuntur
+        quidem nulla ullam voluptas? Illo deserunt nulla corrupti soluta
+        perferendis rerum rem, facere eum eos aperiam unde accusantium!
+        Consequuntur non dolorum inventore odit labore nihil doloremque
+        accusantium quaerat placeat voluptatibus, minus illo cumque nemo ratione
+        fugiat? Vero, aut. Illo incidunt quas ipsam facilis provident laboriosam
+        sed eaque enim repudiandae ratione. Nulla rem accusantium, est sapiente
+        eum ducimus quod ea assumenda repellendus itaque optio officiis sed,
+        doloremque iusto?
+      </p>
+      <p className="text-gray-600 text-2xl pt-8">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloremque,
+        culpa. Dolores voluptatibus eaque eligendi maiores nisi alias quo,
+        exercitationem necessitatibus unde omnis, doloremque sint laudantium
+        doloribus officiis eum, distinctio quos dolorum facilis consequuntur
+        quidem nulla ullam voluptas? Illo deserunt nulla corrupti soluta
+        perferendis rerum rem, facere eum eos aperiam unde accusantium!
+        Consequuntur non dolorum inventore odit labore nihil doloremque
+        accusantium quaerat placeat voluptatibus, minus illo cumque nemo ratione
+        fugiat? Vero, aut. Illo incidunt quas ipsam facilis provident laboriosam
+        sed eaque enim repudiandae ratione. Nulla rem accusantium, est sapiente
+        eum ducimus quod ea assumenda repellendus itaque optio officiis sed,
+        doloremque iusto?
+      </p>
+      <p className="text-gray-600 text-2xl pt-8">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloremque,
+        culpa. Dolores voluptatibus eaque eligendi maiores nisi alias quo,
+        exercitationem necessitatibus unde omnis, doloremque sint laudantium
+        doloribus officiis eum, distinctio quos dolorum facilis consequuntur
+        quidem nulla ullam voluptas? Illo deserunt nulla corrupti soluta
+        perferendis rerum rem, facere eum eos aperiam unde accusantium!
+        Consequuntur non dolorum inventore odit labore nihil doloremque
+        accusantium quaerat placeat voluptatibus, minus illo cumque nemo ratione
+        fugiat? Vero, aut. Illo incidunt quas ipsam facilis provident laboriosam
+        sed eaque enim repudiandae ratione. Nulla rem accusantium, est sapiente
+        eum ducimus quod ea assumenda repellendus itaque optio officiis sed,
+        doloremque iusto?
+      </p>
+      <p className="text-gray-600 text-2xl pt-8">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloremque,
+        culpa. Dolores voluptatibus eaque eligendi maiores nisi alias quo,
+        exercitationem necessitatibus unde omnis, doloremque sint laudantium
+        doloribus officiis eum, distinctio quos dolorum facilis consequuntur
+        quidem nulla ullam voluptas? Illo deserunt nulla corrupti soluta
+        perferendis rerum rem, facere eum eos aperiam unde accusantium!
+        Consequuntur non dolorum inventore odit labore nihil doloremque
+        accusantium quaerat placeat voluptatibus, minus illo cumque nemo ratione
+        fugiat? Vero, aut. Illo incidunt quas ipsam facilis provident laboriosam
+        sed eaque enim repudiandae ratione. Nulla rem accusantium, est sapiente
+        eum ducimus quod ea assumenda repellendus itaque optio officiis sed,
+        doloremque iusto?
+      </p>
+      <p className="text-gray-600 text-2xl pt-8">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloremque,
+        culpa. Dolores voluptatibus eaque eligendi maiores nisi alias quo,
+        exercitationem necessitatibus unde omnis, doloremque sint laudantium
+        doloribus officiis eum, distinctio quos dolorum facilis consequuntur
+        quidem nulla ullam voluptas? Illo deserunt nulla corrupti soluta
+        perferendis rerum rem, facere eum eos aperiam unde accusantium!
+        Consequuntur non dolorum inventore odit labore nihil doloremque
+        accusantium quaerat placeat voluptatibus, minus illo cumque nemo ratione
+        fugiat? Vero, aut. Illo incidunt quas ipsam facilis provident laboriosam
+        sed eaque enim repudiandae ratione. Nulla rem accusantium, est sapiente
+        eum ducimus quod ea assumenda repellendus itaque optio officiis sed,
+        doloremque iusto?
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
